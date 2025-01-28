52be0c4ba3c65ad23dae82b7ff60a5c8a73d8bcf
Thomas Roinas <162818253+ThomasRoinas@users.noreply.github.com>
2025-01-28 16:35:06 +0200

    Create server.h

---
+++ server.h
@@ -0,0 +1,7 @@
+void init_catalog(product catalog[])   
+void parent_orders(product catalog[], int p_socket, int *sum_parag, int *sum_succparag, int *sum_failparag, double *sum_price)
+void child_orders(int client_arithmos)
+void anafora(product catalog[])
+void statistics(int sum_parag, int sum_succparag, int sum_failparag, double sum_price)
+
+//Header file
