6ca14c9d57efa43518d359d09ebd9b9664b06488
ThomasRoinas <roinastom@gmail.com>
2025-01-29 22:23:10 +0200

    Fix child process handling: remove unnecessary continue statement in child_orders function

--- server.c
+++ server.c
@@ -150,12 +150,10 @@ void child_orders(int client_arithmos)
             perror("connect");
             close(p_socket);   //Κλείσιμο του socket για την αποφυγή διαρροής μνήμης
             prosp = prosp - 1;
-            //continue;          //Συνέχιση της επανάληψης σε περίπτωση που δεν είναι δυνατή η σύνδεση του πελάτη με τον server
         }
         else
         {
             break;
-
         }
         }
         arithmos_prod = rand() % 20;    //Παραγωγή τυχαίου αριθμού από το 0 έως το 19 για την επιλογή του προϊόντος που θα αγοράσει ο πελάτης
